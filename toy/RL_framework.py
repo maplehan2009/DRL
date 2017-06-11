c2fa7d168a7a203dbd1f3ecc45a0af8b641ff6b3
Jingtao <maplehan2009@gmail.com>
2017-06-11 11:56:06 +0100

    fix bugs

--- toy/RL_framework.py
+++ toy/RL_framework.py
@@ -3,35 +3,62 @@ import numpy as np
 import matplotlib.pyplot as plt
 from my_cnn_agent import my_cnn_agent
 		
-BATCH_SIZE = 10
+BATCH_SIZE = 4
+
+def stack_image(s):
+    return np.mean(s, axis= 2)
 
 if __name__=='__main__':
-	#make a new game environment, get the deminsion of the action space and state space
-	env = gym.make('MsPacman-v0')
-	n_action = env.action_space.n
-	shape_state = env.observation_space.shape
-	
-	#initialise the agent
-	agent = my_cnn_agent(n_action, shape_state, BATCH_SIZE)
-	
-	count = 0
-	Reward = []
-	N = 10000
-	for i_episode in range(N):
-		Reward_local = []
-		state_prior= env.reset()
-		for _ in range(100):
-			count += 1
-			env.render()
-			action = agent.inference(state_prior)
-			state_post, reward, done, info = env.step(action)
-			if done:
-				reward = -5
-			Reward_local.append(reward)
-			agent.update(state_prior, state_post, reward, action, done)
-			if count % 10 == 0:
-				agent.update_nn()
-			if done:
-				Reward.append(np.sum(Reward_local))
-				break
-	Reward = np.array(Reward)
+    #make a new game environment, get the deminsion of the action space and state space
+    env = gym.make('MsPacman-v0')
+    n_action = env.action_space.n
+    shape_state = env.observation_space.shape
+
+    #initialise the agent
+    agent = my_cnn_agent(n_action, shape_state, BATCH_SIZE)
+
+    N = 10
+    print('Explore Phase')
+    for i_episode in range(N):
+        state_prior = env.reset()
+        state_prior = stack_image(state_prior)
+        for _ in range(2000):
+            env.render()
+            action = agent.inference(state_prior)
+            state_post, reward, done, info = env.step(action)
+            state_post = stack_image(state_post)
+            if done:
+                reward = -5
+            agent.get_memory(state_prior, action, reward, state_post, done)
+            if done:
+                break
+            else:
+                state_prior = state_post
+                 
+    print('Learning Phase')
+    N = 10
+    Reward = []
+    Reward_temp = 0
+    for i_episode in range(N):
+        print(i_episode)
+        Reward.append(Reward_temp)
+        Reward_temp = 0
+        state_prior = env.reset()
+        state_prior = stack_image(state_prior)
+        for _ in range(100):
+            #env.render()
+            action = agent.inference(state_prior)
+            state_post, reward, done, info = env.step(action)
+            state_post = stack_image(state_post)
+            if done:
+                reward = -5
+            Reward_temp += reward
+            agent.get_memory(state_prior, action, reward, state_post, done)
+            agent.train_my_agent()
+            if done:
+                break
+            else:
+                state_prior = state_post
+    Reward = np.array(Reward)
+    plt.scatter(range(N), Reward)
+    plt.show()
--- toy/my_cnn_agent.py
+++ toy/my_cnn_agent.py
@@ -1,100 +1,84 @@
 import tensorflow as tf
-import numpy
+import numpy as np
+
+SEED = 66478
 
 class my_cnn_agent:
     def __init__(self, n_action, shape_state, BATCH_SIZE):
-        self.train_data_node, self.logits, self.conv1_weights, self.conv1_biases,\
-              self.conv2_weights, self.conv2_biases, self.fc1_weights, self.fc1_biases,\
-                      self.fc2_weights, self.fc2_biases = self.createnn(n_action, shape_state, BATCH_SIZE)
-        self.createtrainingmethod(BATCH_SIZE)
+        self.n_action = n_action
+        self.BATCH_SIZE = BATCH_SIZE
+        self.memory = []
+        self.train_data_node, self.logits, self.fc1_weights, self.fc1_biases,\
+                      self.fc2_weights, self.fc2_biases = self.createnn(shape_state)
+        self.createtrainingmethod()
         self.session = tf.InteractiveSession()
         self.session.run(tf.initialize_all_variables())
       
-    def createnn(self, n_action, shape_state, BATCH_SIZE):
-        image_size0, image_size1, num_channels = shape_state
-        train_data_node = tf.placeholder(tf.float32, shape=(BATCH_SIZE, image_size0, image_size1, num_channels))
-        conv1_weights = tf.Variable(
-              tf.truncated_normal([5, 5, NUM_CHANNELS, 32],  # 5x5 filter, depth 32.
-                                  stddev=0.1,
-                                  seed=SEED, dtype=tf.float32))
-        conv1_biases = tf.Variable(tf.zeros([32], dtype=tf.float32))
-        conv2_weights = tf.Variable(tf.truncated_normal(
-              [5, 5, 32, 64], stddev=0.1,
-              seed=SEED, dtype=tf.float32))
-        conv2_biases = tf.Variable(tf.constant(0.1, shape=[64], dtype=tf.float32))
+    def createnn(self, shape_state):
+        image_size0, image_size1, _ = shape_state
+        train_data_node = tf.placeholder(tf.float32, shape=(None, image_size0, image_size1))
         fc1_weights = tf.Variable(  # fully connected, depth 512.
-              tf.truncated_normal([image_size0 // 4 * image_size1 // 4 * 64, 512],
+              tf.truncated_normal([image_size0 * image_size1, 512],
                                   stddev=0.1,
                                   seed=SEED,
                                   dtype=tf.float32))
         fc1_biases = tf.Variable(tf.constant(0.1, shape=[512], dtype=tf.float32))
-        fc2_weights = tf.Variable(tf.truncated_normal([512, n_action],
+        fc2_weights = tf.Variable(tf.truncated_normal([512, self.n_action],
                                                         stddev=0.1,
                                                         seed=SEED,
                                                         dtype=tf.float32))
-        fc2_biases = tf.Variable(tf.constant(
-              0.1, shape=[n_action], dtype=tf.float32))
-        # 2D convolution, with 'SAME' padding (i.e. the output feature map has
-        # the same size as the input). Note that {strides} is a 4D array whose
-        # shape matches the data layout: [image index, y, x, depth].
-        conv1 = tf.nn.conv2d(train_data_node,
-                            conv1_weights,
-                            strides=[1, 1, 1, 1],
-                            padding='SAME')
-        # Bias and rectified linear non-linearity.
-        relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_biases))
-        # Max pooling. The kernel size spec {ksize} also follows the layout of
-        # the data. Here we have a pooling window of 2, and a stride of 2.
-        pool1 = tf.nn.max_pool(relu1,
-                              ksize=[1, 2, 2, 1],
-                              strides=[1, 2, 2, 1],
-                              padding='SAME')
-        conv2 = tf.nn.conv2d(pool1,
-                            conv2_weights,
-                            strides=[1, 1, 1, 1],
-                            padding='SAME')
-        relu2 = tf.nn.relu(tf.nn.bias_add(conv2, conv2_biases))
-        pool2 = tf.nn.max_pool(relu2,
-                              ksize=[1, 2, 2, 1],
-                              strides=[1, 2, 2, 1],
-                              padding='SAME')
+        fc2_biases = tf.Variable(tf.constant(0.1, shape=[self.n_action], dtype=tf.float32))
+        
         # Reshape the feature map cuboid into a 2D matrix to feed it to the
         # fully connected layers.
-        pool_shape = pool2.get_shape().as_list()
-        reshape = tf.reshape(
-            pool2,
-            [pool_shape[0], pool_shape[1] * pool_shape[2] * pool_shape[3]])
+        reshape = tf.reshape(train_data_node, [-1, image_size0 * image_size1])
         # Fully connected layer. Note that the '+' operation automatically
         # broadcasts the biases.
         hidden = tf.nn.relu(tf.matmul(reshape, fc1_weights) + fc1_biases)
         # activations such that no rescaling is needed at evaluation time.
         logits = tf.matmul(hidden, fc2_weights) + fc2_biases
-        return train_data_node, logits, conv1_weights, conv1_biases, conv2_weights, \
-                conv2_biases, fc1_weights, fc1_biases, fc2_weights, fc2_biases
+        return train_data_node, logits, fc1_weights, fc1_biases, fc2_weights, fc2_biases
                             
-    def createtrainingmethod(self, BATCH_SIZE):
-    	#TODO: modify the code here to adapt to Q learning loss function
-        self.train_labels_node = tf.placeholder(tf.int64, shape=(BATCH_SIZE,))
-        self.loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
-                labels=self.train_labels_node, logits=self.logits))
-        self.batch = tf.Variable(0, dtype=tf.float32)
-        # Decay once per epoch, using an exponential schedule starting at 0.01.
-        self.learning_rate = 0.01
-        # Use simple momentum for the optimization.
-        self.optimizer = tf.train.Optimizer(self.learning_rate, 0.9).minimize(self.loss, global_step=self.batch) 
+    def createtrainingmethod(self):
+        self.actionvector = tf.placeholder("float", [None, self.n_action])
+        self.Gt = tf.placeholder("float", [None])
+        Q_action = tf.reduce_sum(tf.multiply(self.actionvector, self.logits), reduction_indices = 1)
+        self.loss = tf.reduce_mean(tf.square(self.Gt - Q_action))
+        self.trainStep = tf.train.RMSPropOptimizer(0.0025, 0.99, 0.0, 1e-6).minimize(self.loss)
         
-    def train_nn(self, batch_data, batch_labels):
-        # This dictionary maps the batch data (as a numpy array) to the
-        # node in the graph it should be fed to.
-        feed_dict = {self.train_data_node: batch_data,
-                   self.train_labels_node: batch_labels}
-        # Run the optimizer to update weights.
-        self.session.run(self.optimizer, feed_dict=feed_dict)
+    def train_my_agent(self):
+        l = list(np.random.choice(len(self.memory), self.BATCH_SIZE, replace=False))
+        minibatch = []
+        for i in l:
+            minibatch.append(self.memory[i])
+            
+        s = [data[0] for data in minibatch]
+        a = [data[1] for data in minibatch]
+        r = [data[2] for data in minibatch]
+        s2 = [data[3] for data in minibatch]
         
-    def inference(self, data):
-        """Get all predictions for a dataset by running it in small batches."""
-        predictions = self.session.run(
-                tf.nn.softmax(self.logits),
-                feed_dict={self.train_data_node: data})
-            predictions[begin:, :] = batch_predictions[begin - size:, :]
-        return np.argmax(predictions)
+        Gt = []
+        Q2 = self.logits.eval(feed_dict = {self.train_data_node : s2})
+        for i in range(self.BATCH_SIZE):
+            terminal = minibatch[i][4]
+            if terminal:
+                Gt.append(r[i])
+            else:
+                Gt.append(r[i] + 0.9 * np.max(Q2[i]))
+        
+        self.trainStep.run(feed_dict = {self.Gt : Gt, self.actionvector : a, self.train_data_node : s})
+        
+    def inference(self, current_state):
+        QValue = self.logits.eval(feed_dict = {self.train_data_node : [current_state]})
+        epsilon = 0.3
+        if np.random.rand() > epsilon:
+        	action = np.argmax(QValue)
+        else:
+        	action = int(np.random.rand() * self.n_action)
+        return action
+    
+    def get_memory(self, s, a, r, s2, terminal):
+        aa = [0] * self.n_action
+        aa[a] = 1
+        self.memory.append((s, aa, r, s2, terminal))
+        
