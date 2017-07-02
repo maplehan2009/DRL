8dc9292760ad0d2834e56499e0b728ee5c50d1d2
Jingtao <maplehan2009@gmail.com>
2017-07-02 20:30:32 +0100

    add some comments

--- A3C/worker.py
+++ A3C/worker.py
@@ -67,7 +67,7 @@ def run(args, server):
                              save_model_secs=30,
                              save_summaries_secs=30)
 
-    num_global_steps = 100000000
+    num_global_steps = 10000
 
     logger.info(
         "Starting session. If this hangs, we're mostly likely waiting to connect to the parameter server. " +
