3ab3b7fdc6ffd96c286858abb731f1de543f4681
Jingtao <maplehan2009@gmail.com>
2017-07-04 23:08:31 +0100

    add plot code

--- A3C/get_data_from_summaries.py
+++ A3C/get_data_from_summaries.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 
 if __name__ == '__main__':
-	inpath0 = '/home/jingtao/Work/DRL_Data/Data_Basic_A3C/events.train0.basic'
-	inpath1 = '/home/jingtao/Work/DRL_Data/Data_Basic_A3C/events.train1.basic'
+	inpath0 = '/home/jingtao/Work/DRL_Data/Data_Basic_A3C/train_0/events.out.tfevents.1499180217.cloud-vm-45-26.doc.ic.ac.uk'
+	inpath1 = '/home/jingtao/Work/DRL_Data/Data_Basic_A3C/train_1/events.out.tfevents.1499180253.cloud-vm-45-26.doc.ic.ac.uk'
 	sg = {event_accumulator.COMPRESSED_HISTOGRAMS: 1,
 		event_accumulator.IMAGES: 1,
         event_accumulator.AUDIO: 1,
@@ -15,17 +15,17 @@ if __name__ == '__main__':
 	ea1 = event_accumulator.EventAccumulator(inpath1, size_guidance=sg)
 	ea0.Reload()
 	ea1.Reload()
-	scalar_tags = ea0.Tags()['scalars']
-	df0 = pd.DataFrame(columns=scalar_tags)
-	df1 = pd.DataFrame(columns=scalar_tags)
-    
-	for tag in scalar_tags:
-		events0 = ea0.Scalars(tag)
-		scalars0 = np.array(list(map(lambda x: x.value, events0)))
-		df0.loc[:, tag] = scalars0
-		events1 = ea1.Scalars(tag)
-		scalars1 = np.array(list(map(lambda x: x.value, events1)))
-		df1.loc[:, tag] = scalars1
+	tags = ['global/episode_reward']
+	df0 = pd.DataFrame(columns=tags)
+	df1 = pd.DataFrame(columns=tags)
+
+	events0 = ea0.Scalars(tags[0])
+	scalars0 = np.array(list(map(lambda x: x.value, events0)))
+	df0.loc[:, tags[0]] = scalars0
+	
+	events1 = ea1.Scalars(tags[0])
+	scalars1 = np.array(list(map(lambda x: x.value, events1)))
+	df1.loc[:, tags[0]] = scalars1
     
 	df0.to_csv('/home/jingtao/Work/DRL_Data/Data_Basic_A3C/agent0_basic_a3c.csv')
 	df1.to_csv('/home/jingtao/Work/DRL_Data/Data_Basic_A3C/agent1_basic_a3c.csv')
--- A3C/plot_graph.py
+++ A3C/plot_graph.py
@@ -1,5 +1,36 @@
 import pandas as pd
+import matplotlib.pylab as plt
+import numpy as np
+plt.style.use('ggplot')
+
+def moving_average(x):
+	N = len(x)
+	window = 8
+	x_new = []
+	for i in range(N-window):
+		x_new.append(np.mean(x[i:i+window]))
+	return np.array(x_new)	
 
 if __name__=='__main__':
 	df0 = pd.read_csv('/home/jingtao/Work/DRL_Data/Data_Basic_A3C/agent0_basic_a3c.csv')
 	df1 = pd.read_csv('/home/jingtao/Work/DRL_Data/Data_Basic_A3C/agent1_basic_a3c.csv')
+	Y0 = df0['global/episode_reward'].values
+	Y1 = df1['global/episode_reward'].values
+	Y0 = moving_average(Y0)
+	Y1 = moving_average(Y1)
+	N0 = Y0.size
+	N1 = Y1.size
+	X0 = range(N0)
+	X1 = range(N1)
+
+	
+	plt.figure(figsize=(8, 8))
+	plt.plot(X0, Y0, X1, Y1, linewidth=1.2)
+	plt.xlabel('Episode')
+	plt.ylabel('Episode Reward')
+	plt.title('Episode Reward v.s. Episode')
+	plt.legend(['Agent0', 'Agent1'])
+	#plt.show()
+	plt.savefig('/home/jingtao/Work/DRL_Data/Data_Basic_A3C/reward.eps', format='eps', dpi=1000)
+	
+	
