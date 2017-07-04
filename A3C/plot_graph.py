7d0068a85b492d176045a7bffb99bd4e947856e0
Jingtao <maplehan2009@gmail.com>
2017-07-04 17:16:03 +0100

    add plot code

---
+++ A3C/plot_graph.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+if __name__=='__main__':
+	df0 = pd.read_csv('/home/jingtao/Work/DRL_Data/Data_Basic_A3C/agent0_basic_a3c.csv')
+	df1 = pd.read_csv('/home/jingtao/Work/DRL_Data/Data_Basic_A3C/agent1_basic_a3c.csv')
