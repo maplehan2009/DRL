2e3cee0124a43666860184e8b2eeea907c29cc6d
Jingtao <maplehan2009@gmail.com>
2017-07-05 22:36:35 +0100

    bug fix

--- A3C/get_data_from_summaries.py
+++ A3C/get_data_from_summaries.py
@@ -2,9 +2,22 @@ from tensorflow.python.summary import event_accumulator
 import numpy as np
 import pandas as pd
 
+def get_df(df, events):
+	rewards = np.array(list(map(lambda x: x.value, events)))
+	steps = np.array(list(map(lambda x: x.step, events)))
+	df.loc[:, tags[0]] = steps
+	df.loc[:, tags[1]] = rewards
+	return
+
 if __name__ == '__main__':
-	inpath0 = '/home/jingtao/Work/DRL_Data/Data_Basic_A3C/train_0/events.out.tfevents.1499180217.cloud-vm-45-26.doc.ic.ac.uk'
-	inpath1 = '/home/jingtao/Work/DRL_Data/Data_Basic_A3C/train_1/events.out.tfevents.1499180253.cloud-vm-45-26.doc.ic.ac.uk'
+	folders = ['Basic', '1level', '2level', '3level']
+	#folder = folders[1]
+	inpath0 = '/home/jingtao/Work/DRL_Data/Data_' + folders[0] + '_A3C/train_0/events.out.tfevents.1499180217.cloud-vm-45-26.doc.ic.ac.uk'
+	inpath1 = '/home/jingtao/Work/DRL_Data/Data_' + folders[0] + '_A3C/train_1/events.out.tfevents.1499180253.cloud-vm-45-26.doc.ic.ac.uk'
+	
+	inpath2 = '/home/jingtao/Work/DRL_Data/Data_' + folders[1] + '_A3C/train_0/events.out.tfevents.1499210582.cloud-vm-47-204.doc.ic.ac.uk'
+	inpath3 = '/home/jingtao/Work/DRL_Data/Data_' + folders[1] + '_A3C/train_1/events.out.tfevents.1499210587.cloud-vm-47-204.doc.ic.ac.uk'
+	
 	sg = {event_accumulator.COMPRESSED_HISTOGRAMS: 1,
 		event_accumulator.IMAGES: 1,
         event_accumulator.AUDIO: 1,
@@ -13,20 +26,32 @@ if __name__ == '__main__':
     
 	ea0 = event_accumulator.EventAccumulator(inpath0, size_guidance=sg)
 	ea1 = event_accumulator.EventAccumulator(inpath1, size_guidance=sg)
+	ea2 = event_accumulator.EventAccumulator(inpath2, size_guidance=sg)
+	ea3 = event_accumulator.EventAccumulator(inpath3, size_guidance=sg)	
+	
 	ea0.Reload()
 	ea1.Reload()
-	tags = ['global/episode_reward']
+	ea2.Reload()
+	ea3.Reload()
+	
+	tags = ['step', 'reward']
 	df0 = pd.DataFrame(columns=tags)
 	df1 = pd.DataFrame(columns=tags)
-
-	events0 = ea0.Scalars(tags[0])
-	scalars0 = np.array(list(map(lambda x: x.value, events0)))
-	df0.loc[:, tags[0]] = scalars0
+	df2 = pd.DataFrame(columns=tags)
+	df3 = pd.DataFrame(columns=tags)
 	
-	events1 = ea1.Scalars(tags[0])
-	scalars1 = np.array(list(map(lambda x: x.value, events1)))
-	df1.loc[:, tags[0]] = scalars1
+	events0 = ea0.Scalars('global/episode_reward')
+	events1 = ea1.Scalars('global/episode_reward')
+	events2 = ea2.Scalars('global/episode_reward')
+	events3 = ea3.Scalars('global/episode_reward')
+	
+	get_df(df0, events0)
+	get_df(df1, events1)
+	get_df(df2, events2)
+	get_df(df3, events3)
     
-	df0.to_csv('/home/jingtao/Work/DRL_Data/Data_Basic_A3C/agent0_basic_a3c.csv')
-	df1.to_csv('/home/jingtao/Work/DRL_Data/Data_Basic_A3C/agent1_basic_a3c.csv')
+	df0.to_csv('/home/jingtao/Work/DRL_Data/Data_' + folders[0] + '_A3C/agent0_' + folders[0] +'_a3c.csv')
+	df1.to_csv('/home/jingtao/Work/DRL_Data/Data_' + folders[0] + '_A3C/agent1_' + folders[0] + '_a3c.csv')
+	df2.to_csv('/home/jingtao/Work/DRL_Data/Data_' + folders[1] + '_A3C/agent0_' + folders[1] +'_a3c.csv')
+	df3.to_csv('/home/jingtao/Work/DRL_Data/Data_' + folders[1] + '_A3C/agent1_' + folders[1] + '_a3c.csv')	
 	
--- A3C/plot_graph.py
+++ A3C/plot_graph.py
@@ -4,33 +4,39 @@ import numpy as np
 plt.style.use('ggplot')
 
 def moving_average(x):
+	x = list(x)
 	N = len(x)
 	window = 8
 	x_new = []
 	for i in range(N-window):
 		x_new.append(np.mean(x[i:i+window]))
+	x_new = x_new + x[N-window : N]
 	return np.array(x_new)	
 
 if __name__=='__main__':
-	df0 = pd.read_csv('/home/jingtao/Work/DRL_Data/Data_Basic_A3C/agent0_basic_a3c.csv')
-	df1 = pd.read_csv('/home/jingtao/Work/DRL_Data/Data_Basic_A3C/agent1_basic_a3c.csv')
-	Y0 = df0['global/episode_reward'].values
-	Y1 = df1['global/episode_reward'].values
-	Y0 = moving_average(Y0)
-	Y1 = moving_average(Y1)
-	N0 = Y0.size
-	N1 = Y1.size
-	X0 = range(N0)
-	X1 = range(N1)
-
+	folders = ['Basic', '1level', '2level', '3level']
+	df0 = pd.read_csv('/home/jingtao/Work/DRL_Data/Data_' + folders[0] + '_A3C/agent0_' + folders[0] +'_a3c.csv')
+	df1 = pd.read_csv('/home/jingtao/Work/DRL_Data/Data_' + folders[0] + '_A3C/agent1_' + folders[0] +'_a3c.csv')
+	df2 = pd.read_csv('/home/jingtao/Work/DRL_Data/Data_' + folders[1] + '_A3C/agent0_' + folders[1] +'_a3c.csv')
+	df3 = pd.read_csv('/home/jingtao/Work/DRL_Data/Data_' + folders[1] + '_A3C/agent1_' + folders[1] +'_a3c.csv')
+	
+	df00 = pd.concat([df0, df1])
+	df00.sort_values(['step', 'reward'], inplace=True)
+	df11 = pd.concat([df2, df3])
+	df11.sort_values(['step', 'reward'], inplace=True)
+	
+	Y0 = moving_average(df00['reward'].values)
+	X0 = df00['step'].values
+	Y1 = moving_average(df11['reward'].values)
+	X1 = df11['step'].values
 	
 	plt.figure(figsize=(8, 8))
 	plt.plot(X0, Y0, X1, Y1, linewidth=1.2)
-	plt.xlabel('Episode')
+	plt.xlabel('Step')
 	plt.ylabel('Episode Reward')
-	plt.title('Episode Reward v.s. Episode')
-	plt.legend(['Agent0', 'Agent1'])
+	plt.title('Episode Reward v.s. Step')
+	plt.legend([folders[0], folders[1]])
 	#plt.show()
-	plt.savefig('/home/jingtao/Work/DRL_Data/Data_Basic_A3C/reward.eps', format='eps', dpi=1000)
+	plt.savefig('/home/jingtao/Work/DRL_Data/result.eps', format='eps', dpi=1000)
 	
 	
